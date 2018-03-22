5745fb667a05b82418baafa8efa05d838ac45a45
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-03-22 10:19:58 +0800

    报名页面git

---
+++ dist/js/index.js
@@ -0,0 +1,29 @@
+//移动端实现button:active
+// document.addEventListener('touchstart',function(){},false);
+$('#protocol').on('change',function() {
+  if($(this).prop("checked")){
+    $('.icon-select').addClass('z-cur')
+  }else{
+    $('.icon-select').removeClass('z-cur')
+  }
+})
+
+$(document).on('click','#signUpBtn',function() {
+ 
+  if(!$('#protocol').prop("checked")){
+    $(document).dialog({      
+      overlayClose:true,
+      titleShow: false,
+      content: '请同意用户协议！',
+    });
+    return 
+  }
+  if(!$('#name').val() || !$('#tel').val()){
+    $(document).dialog({      
+      overlayClose:true,
+      titleShow: false,
+      content: '请填写标 <span style="color:#f00">*</span> 号的信息！',
+    });
+    return 
+  }
+})
