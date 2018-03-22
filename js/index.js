7231ace8295f1a590ef7f314576a78adc7dc5e30
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-03-22 09:45:02 +0800

    刮刮卡页面优化

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 var lottery = new LotteryCard(document.getElementById('js_lottery'),{
-  size: 20, //滑动区域大小
+  size: 10, //滑动区域大小
   percent: 50, //激活百分比到谋个值 就全显示
   resize: true, //canvas的大小是否是可变的
   cover: '#999999' //img or color string, default gray
 });
 lottery.on('start',function(){
   //中奖结果，传递是中奖结果图片地址
-  lottery.setResult('...imageSrc');
+  lottery.setResult('../img/icon-no.png');
 }).on('end',function(){}).on('reset',function(){});
